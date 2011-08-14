8ea86b05a777d7939855185f63fe645b022a5a12
Laurie Harper <laurie@holoweb.net>
2011-08-14 04:57:58 -0400

    Restore 'name' on attribute events; include it in opentag events too for convenience.

--- test/xmlns-strict.js
+++ test/xmlns-strict.js
@@ -16,22 +16,22 @@ require(__dirname).test
       [ [ "opentag", { name: "root", namespace: "", prefix: "", local: "root",
             attributes: {}, bindings: [] } ]
 
-      , [ "attribute", { /*name: "attr", */value: "normal", namespace: "", prefix: "", local: "attr" } ]
+      , [ "attribute", { name: "attr", value: "normal", namespace: "", prefix: "", local: "attr" } ]
       , [ "opentag", { name: "plain", namespace: "", prefix: "", local: "plain", 
-            attributes: { "attr": { value: "normal", namespace: "", prefix: "", local: "attr" } }, 
+            attributes: { "attr": { name: "attr", value: "normal", namespace: "", prefix: "", local: "attr" } }, 
             bindings: [] } ]
       , [ "closetag", "plain" ]
       
       , [ "bindnamespace", { prefix: "", namespace: "uri:default" } ]
       
-      , [ "attribute", { /*name: "xmlns", */value: "uri:default", namespace: "uri:default", prefix: "", local: "xmlns" } ]
+      , [ "attribute", { name: "xmlns", value: "uri:default", namespace: "uri:default", prefix: "", local: "xmlns" } ]
       , [ "opentag", { name: "ns1", namespace: "uri:default", prefix: "", local: "ns1", 
-            attributes: { "xmlns": { value: "uri:default", namespace: "uri:default", prefix: "", local: "xmlns" } }, 
+            attributes: { "xmlns": { name: "xmlns", value: "uri:default", namespace: "uri:default", prefix: "", local: "xmlns" } }, 
             bindings: [ "" ] } ]
 
-      , [ "attribute", { /*name: "attr", */value: "normal", namespace: "uri:default", prefix: "", local: "attr" } ]                     // XXX default NS shouldn't apply to attrs
+      , [ "attribute", { name: "attr", value: "normal", namespace: "uri:default", prefix: "", local: "attr" } ]                     // XXX default NS shouldn't apply to attrs
       , [ "opentag", { name: "plain", namespace: "uri:default", prefix: "", local: "plain", 
-            attributes: { "attr": { value: "normal", namespace: "uri:default", prefix: "", local: "attr" } },   // XXX default NS shouldn't apply to attrs
+            attributes: { "attr": { name: "attr", value: "normal", namespace: "uri:default", prefix: "", local: "attr" } },   // XXX default NS shouldn't apply to attrs
             bindings: [] } ]
       , [ "closetag", "plain" ]
 
@@ -41,20 +41,20 @@ require(__dirname).test
       
       , [ "bindnamespace", { prefix: "a", namespace: "uri:nsa" } ]
       
-      , [ "attribute", { /*name: "xmlns:a", */value: "uri:nsa", namespace: "", prefix: "xmlns", local: "a" } ]
+      , [ "attribute", { name: "xmlns:a", value: "uri:nsa", namespace: "", prefix: "xmlns", local: "a" } ]
       , [ "opentag", { name: "ns2", namespace: "", prefix: "", local: "ns2", 
-            attributes: { "xmlns:a": { value: "uri:nsa", namespace: "", prefix: "xmlns", local: "a" } }, 
+            attributes: { "xmlns:a": { name: "xmlns:a", value: "uri:nsa", namespace: "", prefix: "xmlns", local: "a" } }, 
             bindings: [ "a" ] } ]
 
-      , [ "attribute", { /*name: "attr", */value: "normal", namespace: "", prefix: "", local: "attr" } ]
+      , [ "attribute", { name: "attr", value: "normal", namespace: "", prefix: "", local: "attr" } ]
       , [ "opentag", { name: "plain", namespace: "", prefix: "", local: "plain", 
-            attributes: { "attr": { value: "normal", namespace: "", prefix: "", local: "attr" } },
+            attributes: { "attr": { name: "attr", value: "normal", namespace: "", prefix: "", local: "attr" } },
             bindings: [] } ]
       , [ "closetag", "plain" ]
       
-      , [ "attribute", { /*name: "a:attr", */value: "namespaced", namespace: "uri:nsa", prefix: "a", local: "attr" } ]
+      , [ "attribute", { name: "a:attr", value: "namespaced", namespace: "uri:nsa", prefix: "a", local: "attr" } ]
       , [ "opentag", { name: "a:ns", namespace: "uri:nsa", prefix: "a", local: "ns", 
-            attributes: { "a:attr": { value: "namespaced", namespace: "uri:nsa", prefix: "a", local: "attr" } },
+            attributes: { "a:attr": { name: "a:attr", value: "namespaced", namespace: "uri:nsa", prefix: "a", local: "attr" } },
             bindings: [] } ]
       , [ "closetag", "a:ns" ]
 
